7312c65b800292b2d429bb00e776dd610a0ea395
Reza Hashemi <reza.8139@gmail.com>
2025-08-01 01:56:30 +0330

    edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { PublicModule } from './public/public.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PublicModule,
+    RouterModule.register([
+      {
+        path: 'api/public',
+        module: PublicModule,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
