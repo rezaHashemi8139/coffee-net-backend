a3dc27c810ec39b5fc374efc8eb604f5b2a449ae
Reza Hashemi <reza.8139@gmail.com>
2025-08-04 12:51:46 +0330

    add routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PublicModule } from './public/public.module';
+import { ClientModule } from './client/client.module';
+import { AdminModule } from './admin/admin.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     PublicModule,
+    ClientModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
+  app.setGlobalPrefix('api');
   console.log(`app listen on http://localhost:${port}`);
 }
 void bootstrap();
